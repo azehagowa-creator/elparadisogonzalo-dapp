35c5bbb2f6d393d3cb8ea8769f99cddb4d50b1d7
koagonzalo11 <azehagowa@elparadisogonzalo.net>
2026-09-24 15:03:44 +0100

    approve and apply changes

--- contracts/ElparadisogonzaloContract.sol
+++ contracts/ElparadisogonzaloContract.sol
@@ -6,11 +6,11 @@ pragma solidity >=0.7.0 <0.8.0;
  * @title Storage
  * @dev Store & retrieve value in a variable
  */
-contract BadContract {
+contract ElparadisogonzaloContract {
 
     uint256 number;
     
     function divide() public view returns (uint256){
-        return 3/number;
+        return all/number;
     }
 }
